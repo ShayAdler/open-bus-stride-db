ac50a9067702d98365982d44aa7ea6b134a21e12
Ori Hoch <ori@uumpa.com>
2021-10-17 20:48:01 +0300

    update ride_stop table to Ride and Stop foreign keys

--- open_bus_stride_db/model/ride_stop.py
+++ open_bus_stride_db/model/ride_stop.py
@@ -6,8 +6,10 @@ import sqlalchemy.orm
 class RideStop(Base):
     __tablename__ = 'ride_stop'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    route_stop_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('route_stop.id'), index=True)
-    route_stop = sqlalchemy.orm.relationship('RouteStop', back_populates='ride_stops')
+    stop_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('stop.id'), index=True)
+    stop = sqlalchemy.orm.relationship('Stop', back_populates='ride_stops')
+    ride_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('ride.id'), index=True)
+    ride = sqlalchemy.orm.relationship('Ride', back_populates='ride_stops')
     is_from_gtfs = sqlalchemy.Column(sqlalchemy.Boolean, index=True)
     gtfs_arrival_datetime = sqlalchemy.Column(sqlalchemy.DateTime)
     gtfs_departure_datetime = sqlalchemy.Column(sqlalchemy.DateTime)
