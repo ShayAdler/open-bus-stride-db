368ef4ca8b6916ad1a502b9445cd47e27aa778b4
Aviv Sela <avivsela10@gmail.com>
2022-01-13 09:27:26 +0200

    Define some gtfs tables with cascade delete option (#9)
    * add cascade delete for some tables, so upsert operation will be more straight forward
    
    * add cascade delete for the gtfs_stop table in case records in gtfs route stop will be deleted

--- open_bus_stride_db/model/gtfs_route_stop.py
+++ open_bus_stride_db/model/gtfs_route_stop.py
@@ -7,7 +7,7 @@ class GtfsRouteStop(Base):
     __tablename__ = 'gtfs_route_stop'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     gtfs_stop_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('gtfs_stop.id'), index=True)
-    gtfs_stop = sqlalchemy.orm.relationship('GtfsStop', back_populates='gtfs_route_stops')
+    gtfs_stop = sqlalchemy.orm.relationship('GtfsStop', back_populates='gtfs_route_stops', cascade="all, delete")
     gtfs_route_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('gtfs_route.id'), index=True)
     gtfs_route = sqlalchemy.orm.relationship('GtfsRoute', back_populates='gtfs_route_stops')
     order = sqlalchemy.Column(sqlalchemy.Integer, index=True)
