e88c1442cf81b400d5261a7331b0104894877459
Ori Hoch <ori@uumpa.com>
2022-01-17 17:45:21 +0200

    add gtfs_ride first/last stop and star/end time

--- open_bus_stride_db/model/gtfs_ride.py
+++ open_bus_stride_db/model/gtfs_ride.py
@@ -10,3 +10,9 @@ class GtfsRide(Base):
     gtfs_route = sqlalchemy.orm.relationship('GtfsRoute', back_populates='gtfs_rides')
     journey_ref = sqlalchemy.Column(sqlalchemy.String, index=True)
     gtfs_ride_stops = sqlalchemy.orm.relationship('GtfsRideStop', back_populates='gtfs_ride')
+
+    # populated from open_bus_stride_etl.gtfs.update_ride_aggregations
+    first_gtfs_ride_stop_id = sqlalchemy.Column(sqlalchemy.Integer)
+    last_gtfs_ride_stop_id = sqlalchemy.Column(sqlalchemy.Integer)
+    start_time = sqlalchemy.Column(DateTimeWithTimeZone)
+    end_time = sqlalchemy.Column(DateTimeWithTimeZone)
