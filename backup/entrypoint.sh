d40e9e4cbac929ba0521284644206f7ae09ec5b3
Ori Hoch <ori@uumpa.com>
2022-02-15 14:54:45 +0200

    improvements to backup script

--- backup/entrypoint.sh
+++ backup/entrypoint.sh
@@ -7,12 +7,30 @@
 
 if [ "${1}" == "--cron" ]; then
   mkdir -p /srv/crontab &&\
-  echo "${2} /srv/entrypoint.sh" > /srv/crontab/root
+  echo "${2} /srv/entrypoint.sh --from-cron 2>&1" > /srv/crontab/root
   echo >> /srv/crontab/root
   exec crond -f -L /dev/stdout -c /srv/crontab
 else
-  cd `mktemp -d` &&\
-  pg_dump -d $DB -h $HOSTNAME -U $USER -n $SCHEMA --no-privileges -f ./stride_db.sql &&\
-  gzip ./stride_db.sql &&\
-  mv ./stride_db.sql.gz $FILENAME
+  mkdir -p /srv/backup &&\
+  cd /srv/backup &&\
+  echo Running pg_dump from hostname $PG_BACKUP_HOSTNAME schema $PG_BACKUP_CRON_SCHEDULE db $PG_PG_BACKUP_DB to $PG_BACKUP_FILENAME &&\
+  pg_dump -d $PG_BACKUP_DB -h $PG_BACKUP_HOSTNAME -U $PG_BACKUP_USER -n $PG_BACKUP_SCHEMA --no-privileges -f ./stride_db.sql &&\
+  du -h ./stride_db.sql &&\
+  gzip -kf ./stride_db.sql &&\
+  du -h ./stride_db.sql.gz &&\
+  cp -f ./stride_db.sql.gz "${PG_BACKUP_FILENAME}_temp" &&\
+  du -h "${PG_BACKUP_FILENAME}_temp" &&\
+  mv "${PG_BACKUP_FILENAME}_temp" "${PG_BACKUP_FILENAME}" &&\
+  du -h "${PG_BACKUP_FILENAME}"
+  if [ "$?" != "0" ]; then
+    echo "Disappointing failure"
+    if [ "${1}" == "--from-cron" ]; then
+      echo terminating the crond process
+      rm -rf /srv/crontab
+    fi
+    exit 1
+  else
+    echo "Great success"
+    exit 0
+  fi
 fi
