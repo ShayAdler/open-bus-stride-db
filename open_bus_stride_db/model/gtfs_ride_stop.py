bb321219d1df0b998168962d783306d364e5b0c0
Shay <61648359+ShayAdler@users.noreply.github.com>
2022-06-12 13:56:24 +0300

    Added indexes for routes timetable (#15)
    * Added indexes
    
    * fix index in gtfs_ride
    
    * fix index in gtfs_ride_stop
    
    Co-authored-by: Shay <shay@bionicstork.com>
    Co-authored-by: Ori Hoch <ori@uumpa.com>

--- open_bus_stride_db/model/gtfs_ride_stop.py
+++ open_bus_stride_db/model/gtfs_ride_stop.py
@@ -8,11 +8,16 @@ from open_bus_stride_db.model.base import DateTimeWithTimeZone, info
 class GtfsRideStop(Base):
     __tablename__ = 'gtfs_ride_stop'
     __table_args__ = (
+        sqlalchemy.Index(
+            'idx_gtfs_ride_stop_arrival_time',
+            sqlalchemy.text("date_trunc('day', arrival_time)::date")
+        ),
         {**info("""
             A planned stop along a [[gtfs_ride]]. 
             Populated daily from the MOT GTFS data by [[gtfs-etl]]. 
-        """)}
+        """)},
     )
+
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     gtfs_stop_id = sqlalchemy.Column(
         sqlalchemy.Integer, sqlalchemy.ForeignKey('gtfs_stop.id'), index=True,
