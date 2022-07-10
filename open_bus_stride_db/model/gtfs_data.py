88caa8555557d2e6366d8cab76d189a200b6b92a
Ori Hoch <ori@uumpa.com>
2022-07-10 13:36:33 +0300

    update gtfs_data table

--- open_bus_stride_db/model/gtfs_data.py
+++ open_bus_stride_db/model/gtfs_data.py
@@ -12,12 +12,15 @@ class GtfsData(Base):
         """)},
     )
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    date = sqlalchemy.Column(sqlalchemy.Date, index=True)
+    date = sqlalchemy.Column(sqlalchemy.Date, index=True, unique=True)
+    download_upload_started_at = sqlalchemy.Column(DateTimeWithTimeZone, index=True)
+    download_upload_completed_at = sqlalchemy.Column(DateTimeWithTimeZone)
+    download_upload_error = sqlalchemy.Column(sqlalchemy.String)
+    download_upload_success = sqlalchemy.Column(sqlalchemy.Boolean)
     processing_started_at = sqlalchemy.Column(DateTimeWithTimeZone, index=True)
     processing_completed_at = sqlalchemy.Column(DateTimeWithTimeZone)
     processing_error = sqlalchemy.Column(sqlalchemy.String)
     processing_success = sqlalchemy.Column(sqlalchemy.Boolean)
-    upload_success = sqlalchemy.Column(sqlalchemy.Boolean)
     processing_used_stride_date = sqlalchemy.Column(sqlalchemy.Date, **info("""
         If the GTFS data was processed using a different date then in the date field, this is the date used. 
     """))
