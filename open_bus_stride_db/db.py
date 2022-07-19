5734911b196b3f599ee031c23d960499fa6abb8c
Ori Hoch <ori@uumpa.com>
2022-07-19 17:58:24 +0300

    add sqlalchemy pool configuration options + defaults

--- open_bus_stride_db/db.py
+++ open_bus_stride_db/db.py
@@ -6,10 +6,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
 
-engine = create_engine(os.environ.get('SQLALCHEMY_URL', 'postgresql://postgres:123456@localhost'),
-                       future=True,
-                       connect_args={"options": "-c timezone=utc"},
-                       echo=bool(os.environ.get('SQLALCHEMY_ECHO')))
+engine = create_engine(
+    os.environ.get('SQLALCHEMY_URL', 'postgresql://postgres:123456@localhost'),
+    future=True,
+    connect_args={"options": "-c timezone=utc"},
+    echo=bool(os.environ.get('SQLALCHEMY_ECHO')),
+    pool_size=int(os.environ.get('SQLALCHEMY_POOL_SIZE', 10)),
+    max_overflow=int(os.environ.get('SQLALCHEMY_MAX_OVERFLOW', 20)),
+    pool_timeout=int(os.environ.get('SQLALCHEMY_POOL_TIMEOUT', 30)),
+)
 _sessionmaker = sessionmaker(bind=engine, future=True, autoflush=False, autocommit=False)
 
 
